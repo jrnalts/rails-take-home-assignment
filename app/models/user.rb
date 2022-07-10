a9632b3ce55678b81960a1c49793c0c24da46d4e
andrew <andrew@jvdiamondtech.com>
2022-07-10 18:02:37 +0800

    feat: user password encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   has_many :track_lists
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  before_create :encrypt_password
+
+  def self.login(*args)
+    if args[:email] && args[:password]
+      find_by(
+        email: args[:email],
+        password: EncryptionService.decrypt(args[:password])
+      )
+    end
+  end
+
+  def encrypt_password
+    self.password = EncryptionService.encrypt(password)
+  end
 end
